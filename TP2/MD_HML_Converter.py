9275c613c7ca386956b2edea5bd2cca3cbe5a0de
Goncalini <goncalodaniel28@gmail.com>
2024-02-18 20:39:24 +0000

    tpc2 sem o readme

---
+++ TP2/MD_HML_Converter.py
@@ -0,0 +1,72 @@
+import re
+
+def replacertitle(match):
+    counter = len(match.group(1))
+    replace = f'<h{counter}>{match.group(2)}</h{counter}>'
+    return replace
+
+def replacerlist(match):
+    items = f'<li>\{match.group(2)}</li>'
+    #replace = f'<ol>\n{items}\n</ol>'
+    return items
+
+def markdown_to_html(markdown):
+
+    # Título
+    markdown = re.sub(r'^(#+)\s+(.*)$', replacertitle, markdown, flags=re.MULTILINE)
+    
+    # Bold
+    markdown = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', markdown)
+    
+    # Itálico
+    markdown = re.sub(r'\*(.*?)\*', r'<i>\1</i>', markdown)
+    
+    # Lista numerada
+    markdown = re.sub(r'^(\d+\.)\s*(.*)$', r'<li>\2</li>', markdown, flags=re.MULTILINE)
+    #markdownL = f'<ol>\n{markdown}</ol>'
+
+    #imagem (a ordem importa pq esta expressao regular é mais especifica que a dos link)
+    markdown = re.sub(r'!\[(.*?)\]\((.*?)\)', r'<img src="\2" alt="\1"/>', markdown)
+
+    # Link
+    markdown = re.sub(r'\[(.*?)\]\((.*?)\)', r'<a href="\2">\1</a>', markdown)
+    
+
+    
+
+    
+    '''
+    if '<li>' in markdown:
+        return markdownL
+    else:
+        return markdown
+    '''
+    return markdown
+
+def main():
+    markdown = ""
+    print("Insere Texto Markdown(Se for uma lista, coloca fim na ultima linha para terminar): ")
+    while True:
+        line = input()
+        html = markdown_to_html(line)
+        if '<li>' in html:
+            markdown += line + "\n"
+            while True:
+                line = input()
+                if line.strip().lower() == "fim":
+                    markdown = markdown[:-1]
+                    break
+                markdown += line + "\n"
+            html = markdown_to_html(markdown)
+            print("Texto HTML:")
+            print('<ol>')
+            print(html)
+            print('</ol>')
+            print("Insere Texto Markdown(Se for uma lista, coloca fim na ultima linha para terminar): ")
+        else: 
+            print("Texto HTML:")
+            print(html)   
+            print("Insere Texto Markdown(Se for uma lista, coloca fim na ultima linha para terminar): ")
+
+if __name__ == '__main__':
+    main()
--- TP2/README.md
+++ TP2/README.md
@@ -1,2 +1,27 @@
-# PL2024
-TPCS e restantes projetos de Processamento de Linguagens 23/24
+Criar em Python um pequeno conversor de MarkDown para HTML, o codigo deve receber como input uma linha em markdown e converter para a devida em python
+Cabeçalhos: linhas iniciadas por "# texto", ou "## texto" ou "### texto"
+In: # Exemplo
+Out: <h1>Exemplo</h1>
+Bold: pedaços de texto entre "**":
+In: Este é um **exemplo** ...
+Out: Este é um <b>exemplo</b> ...
+Itálico: pedaços de texto entre "*":
+In: Este é um *exemplo* ...
+Out: Este é um <i>exemplo</i> ...
+Lista numerada:
+In:
+1. Primeiro item
+2. Segundo item
+3. Terceiro item
+Out:
+<ol>
+<li>Primeiro item</li>
+<li>Segundo item</li>
+<li>Terceiro item</li>
+</ol>
+Link: [texto](endereço URL)
+In: Como pode ser consultado em [página da UC](http://www.uc.pt)
+Out: Como pode ser consultado em <a href="http://www.uc.pt">página da UC</a>
+Imagem: ![texto alternativo](path para a imagem)
+In: Como se vê na imagem seguinte: ![imagem dum coelho](http://www.coellho.com) ...
+Out: Como se vê na imagem seguinte: <img src="http://www.coellho.com" alt="imagem dum coelho"/> ...
